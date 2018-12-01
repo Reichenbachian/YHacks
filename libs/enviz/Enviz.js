5e178eceb0c4f550e9186b13ddbc6d71d75a2cf4
Andrew Sun <me@andrewsun.com>
2018-12-01 16:28:44 -0500

    convert to class

--- libs/enviz/Enviz.js
+++ libs/enviz/Enviz.js
@@ -1,107 +1,140 @@
-function Enviz() {
-	// Setup THREE to be treated as a DOM object
-	THREE.Object3D.prototype.appendChild = function (c) { this.add(c); return c; };
-	THREE.Object3D.prototype.querySelectorAll = function () {return this.children; };
-	THREE.Object3D.prototype.insertBefore = function (newNode, referenceNode) {
-		t = newNode;
-		if (referenceNode == null) {
-			console.log(newNode, referenceNode);
-			this.add(newNode);
-		} else {
-			console.log(newNode, referenceNode);
-			referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-		}
-	};
-	// this one is to use D3's .attr() on THREE's objects
-	THREE.Object3D.prototype.setAttribute = function (name, value) {
-	    var chain = name.split('.');
-	    var object = this;
-	    for (var i = 0; i < chain.length - 1; i++) {
-	        object = object[chain[i]];
-	    }
-	    object[chain[chain.length - 1]] = value;
-	}
-
-	// Define global vars
-	var renderer, scene;
-	// player object: only manipulate this position & orientation, don't modify camera directly
-	var player, head;
-	var initialPos = { x: 0, y: 0, z: 30 };
-
-	var vrHMD, vrPlayerController;
-	var options = {
-		scale: 1, // eye separation (IPD) scale
-		posScale: 10 // positional tracking scale
-	};
+// Setup THREE to be treated as a DOM object
+THREE.Object3D.prototype.appendChild = function (c) { this.add(c); return c; };
+THREE.Object3D.prototype.querySelectorAll = function () {return this.children; };
+THREE.Object3D.prototype.insertBefore = function (newNode, referenceNode) {
+    t = newNode;
+    if (referenceNode == null) {
+        console.log(newNode, referenceNode);
+        this.add(newNode);
+    } else {
+        console.log(newNode, referenceNode);
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+};
+// this one is to use D3's .attr() on THREE's objects
+THREE.Object3D.prototype.setAttribute = function (name, value) {
+    var chain = name.split('.');
+    var object = this;
+    for (var i = 0; i < chain.length - 1; i++) {
+        object = object[chain[i]];
+    }
+    object[chain[chain.length - 1]] = value;
+}
 
 
-	function onWindowResize() {
-	    camera.aspect = window.innerWidth / window.innerHeight;
-	    camera.updateProjectionMatrix();
-	    renderer.setSize( window.innerWidth, window.innerHeight );
-	}
+class Enviz {
 
-	function initializeVR() {
-	    renderer = new THREE.WebGLRenderer();
-	    renderer.setSize( window.innerWidth, window.innerHeight );
-	    document.body.appendChild( renderer.domElement );
-	    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	    camera.position.z = 400;
-	    
-	    scene = new THREE.Scene();
-	    
-	    var light = new THREE.DirectionalLight( 0xffffff );
-	    light.position.set( 0, 0, 1 );
-	    scene.add( light );
-	    var geometry = new THREE.CubeGeometry( 20, 20, 20 );
-	    
-	    
-	    // create container for our 3D chart
-	    chart3d = new THREE.Object3D();
-		chart3d.rotation.x = 0.6;
-		scene.add( chart3d );
-	        
-	    window.addEventListener( 'resize', onWindowResize, false );
+    // Define global vars
+    renderer;
+    scene;
+    // player object: only manipulate this position & orientation, don't modify camera directly
+    player;
+    head;
 
-	}
-	function loadData(file, func) {
-		d3.json(file, func);
-	}
+    camera;
 
-	function scatterPlot(columnX, columnY, columnZ, legend=true) {
+    clock = new THREE.Clock();
 
-	}
+    initialPos = { x: 0, y: 0, z: 30 };
 
-	function load( error ) {
-		if ( error ) {
-			console.log( error );
-		}
+    vrHMD;
+    vrPlayerController;
+    options = {
+        scale: 1, // eye separation (IPD) scale
+        posScale: 10 // positional tracking scale
+    };
 
-		init();
-		animate();
-	}
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
 
-	function animate( t ){
-		requestAnimationFrame( animate );
+    generatePlotArea() {
+        const xAxisMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
+        const yAxisMaterial = new THREE.MeshStandardMaterial({color: 0x00ff00});
+        const zAxisMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff});
 
-		var dt = clock.getDelta();
+        const axisGeom = new THREE.CylinderGeometry(0.01, 0.01, 4, 4, 1, true);
 
-		update( dt );
-		render( dt );
-	}
+        const xAxis = new THREE.Mesh(axisGeom, xAxisMaterial);
+        xAxis.rotation.z = Math.PI / 2;
+        const yAxis = new THREE.Mesh(axisGeom, yAxisMaterial);
+        const zAxis = new THREE.Mesh(axisGeom, zAxisMaterial);
+        zAxis.rotation.x = Math.PI / 2;
 
-	function update() {
-		// updates player head position and orientation based on HMD
-		vrPlayerController.update( dt );
-	}
+        const group = new THREE.Group();
+        group.add(xAxis);
+        group.add(yAxis);
+        group.add(zAxis);
 
-	function render( dt ) {
-		vrPlayerController.render( scene );
-	}
-	Enviz.prototype.initializeVR = initializeVR;
-	Enviz.prototype.loadData = loadData;
-	Enviz.prototype.update = update;
-	Enviz.prototype.scatterPlot = scatterPlot;
+        return group;
+    }
+
+    initializeVR() {
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( this.renderer.domElement );
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+        this.camera.position.z = 400;
+
+        this.scene = new THREE.Scene();
+
+        var light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0, 0, 1 );
+        this.scene.add( light );
+        var geometry = new THREE.CubeGeometry( 20, 20, 20 );
+
+
+        // create container for our 3D chart
+        chart3d = new THREE.Object3D();
+        chart3d.rotation.x = 0.6;
+        this.scene.add( chart3d );
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+    }
+
+    loadData(file, func) {
+        d3.json(file, func);
+    }
+
+    scatterPlot(columnX, columnY, columnZ, legend=true) {
+
+    }
+
+    init() {
+        // TODO: Stub
+    }
+
+    load( error ) {
+        if ( error ) {
+            console.log( error );
+        }
+
+        // this.init();
+        this.animate();
+    }
+
+    animate( t ){
+        // TODO
+
+        requestAnimationFrame( this.animate );
+
+        var dt = this.clock.getDelta();
+
+        this.update( dt );
+        this.render( dt );
+    }
+
+    update() {
+        // updates player head position and orientation based on HMD
+        this.vrPlayerController.update( dt );
+    }
+
+    render( dt ) {
+        this.vrPlayerController.render( scene );
+    }
 }
 
 var enviz = new Enviz();
